6cbce39c61ca3121da3fefa991f841a909bd8163
lunarised <lunarised@outlook.com>
2023-12-08 00:28:42 +1300

    Day01 done and tests added

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 pub fn day01a(puzzle_input: &str) -> i32 {
     let mut calibration_sum = 0;
     let calibration_lines = puzzle_input.lines();
@@ -10,44 +8,50 @@ pub fn day01a(puzzle_input: &str) -> i32 {
                 sub_string.push(letter);
             }
         }
-        let mut outString = String::from("");
-        // outString.push(sub_string.chars().next().unwrap());
-        // outString.push(sub_string.chars().last().unwrap());
-        // calibration_sum += outString.parse::<i32>().unwrap();
+        let mut out_string = String::from("");
+        out_string.push(sub_string.chars().next().unwrap());
+        out_string.push(sub_string.chars().last().unwrap());
+        calibration_sum += out_string.parse::<i32>().unwrap();
     }
-    tokenise_number_words(puzzle_input);
     return calibration_sum;
 }
 
 pub fn day01b(puzzle_input: &str) -> i32 {
-    return 32;
-}
-
-fn tokenise_number_words(unprocessed_line: &str) -> &str {
-    let mut enumerableLine = unprocessed_line.lines();
-
+    let enumerable_line = puzzle_input.lines();
     let nums = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
-    let mut resultSum = 0;
-    for line in enumerableLine {
-        let mut ind = 0;
+    let mut result_sum = 0;
+    for line in enumerable_line {
         let mut number_vector: Vec<i32> = Vec::new();
-        for char in line.chars() {
+        for (ind, char) in line.chars().enumerate() {
             if char.is_digit(10) {
                 number_vector.push(char.to_digit(10).unwrap() as i32);
             } else {
                 for num in nums {
-                    if line.find(num).unwrap_or(100000) == ind {
-                        number_vector.push(nums.iter().position(|&n| n == num).unwrap() as i32)
+                    if line[ind..].find(num).unwrap_or(100000) == 0 {
+                        number_vector.push(nums.iter().position(|&n| n == num).unwrap() as i32 + 1)
                     }
                 }
             }
-            ind += 1;
         }
-        println!("{:?}", number_vector);
-        resultSum += ((number_vector.first().unwrap() * 10) + number_vector.last().unwrap())
+        result_sum += (number_vector.first().unwrap() * 10) + number_vector.last().unwrap()
     }
-    println!("{}", resultSum);
-    return "wow";
+    return result_sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils;
+    #[test]
+    fn part1_test() {
+        let day01_test_input = utils::read_file("inputs/01atest.txt".to_string());
+        assert_eq!(day01a(day01_test_input.as_str()), 142);
+    }
+    #[test]
+    fn part2_test() {
+        let day02_test_input = utils::read_file("inputs/01btest.txt".to_string());
+        assert_eq!(day01b(day02_test_input.as_str()), 281);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod day01;
 mod utils;
 fn main() {
     let text_input = utils::read_file("inputs/01a.txt".to_string());
-    println!("{}", day01::day01a(text_input.as_str()))
+    println!("{}", day01::day01a(text_input.as_str()));
+    println!("{}", day01::day01b(text_input.as_str()));
 }
